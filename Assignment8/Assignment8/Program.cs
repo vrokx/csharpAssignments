e131ca4b78ec33d32ba90b52f3ba278b5e71e1de
Vaibhav Raval <vaibhavraval9819@gmail.com>
2022-11-01 13:03:59 +0530

    updated program

--- Assignment8/Assignment8/Program.cs
+++ Assignment8/Assignment8/Program.cs
@@ -1,216 +1,57 @@
-﻿// Code for building SimpleType.dll.
-using System;
+using System.Diagnostics;
 using System.Reflection;
-using System.Globalization;
-using Simple_Type;
+using System.IO;
 
-namespace Simple_Type
+
+namespace Reflection_Assembly
 {
-    public class MySimpleClass
+    internal class Program
     {
-        public void MyMethod(string str, int i)
+        static void Main(string[] args)
         {
-            Console.WriteLine("MyMethod parameters: {0}, {1}", str, i);
-        }
+            Assembly asm = Assembly.LoadFrom(@"C:\Users\vrokx\source\repos\Reflection_Assembly\MyAssembly\bin\Debug\net6.0\MyAssembly.dll");
+            Type t = asm.GetType("MyAssembly.MyClass");
 
-        public void MyMethod(string str, int i, int j)
-        {
-            Console.WriteLine("MyMethod parameters: {0}, {1}, {2}",
-                str, i, j);
-        }
-    }
-}
-
-namespace Custom_Binder
-{
-    class MyMainClass
-    {
-        static void Main()
-        {
-            // Get the type of MySimpleClass.
-            Type myType = typeof(MySimpleClass);
-
-            // Get an instance of MySimpleClass.
-            MySimpleClass myInstance = new MySimpleClass();
-            MyCustomBinder myCustomBinder = new MyCustomBinder();
-
-            // Get the method information for the particular overload
-            // being sought.
-            MethodInfo myMethod = myType.GetMethod("MyMethod",
-                BindingFlags.Public | BindingFlags.Instance,
-                myCustomBinder, new Type[] {typeof(string),
-                typeof(int)}, null);
-            Console.WriteLine(myMethod.ToString());
-
-            // Invoke the overload.
-            myType.InvokeMember("MyMethod", BindingFlags.InvokeMethod,
-                myCustomBinder, myInstance,
-                new Object[] { "Testing...", (int)32 });
-        }
-    }
-
-    // ****************************************************
-    //  A simple custom binder that provides no
-    //  argument type conversion.
-    // ****************************************************
-    class MyCustomBinder : Binder
-    {
-        public override MethodBase BindToMethod(
-            BindingFlags bindingAttr,
-            MethodBase[] match,
-            ref object[] args,
-            ParameterModifier[] modifiers,
-            CultureInfo culture,
-            string[] names,
-            out object state)
-        {
-            if (match == null)
+            
+            var methodInfo = t.GetMethod("MyMethod", new Type[] { typeof(int), typeof(string) });
+            if (methodInfo == null)
             {
-                throw new ArgumentNullException("match");
-            }
-            // Arguments are not being reordered.
-            state = null;
-            // Find a parameter match and return the first method with
-            // parameters that match the request.
-            foreach (MethodBase mb in match)
-            {
-                ParameterInfo[] parameters = mb.GetParameters();
-
-                if (ParametersMatch(parameters, args))
-                {
-                    return mb;
-                }
-            }
-            return null;
-        }
-
-        public override FieldInfo BindToField(BindingFlags bindingAttr,
-            FieldInfo[] match, object value, CultureInfo culture)
-        {
-            if (match == null)
-            {
-                throw new ArgumentNullException("match");
-            }
-            foreach (FieldInfo fi in match)
-            {
-                if (fi.GetType() == value.GetType())
-                {
-                    return fi;
-                }
-            }
-            return null;
-        }
-
-        public override MethodBase SelectMethod(
-            BindingFlags bindingAttr,
-            MethodBase[] match,
-            Type[] types,
-            ParameterModifier[] modifiers)
-        {
-            if (match == null)
-            {
-                throw new ArgumentNullException("match");
+                
+                throw new Exception("MethodnotFound");
             }
 
-            // Find a parameter match and return the first method with
-            // parameters that match the request.
-            foreach (MethodBase mb in match)
+            
+            object[] constructorParameters = new object[2];
+            constructorParameters[0] = 100; 
+            constructorParameters[1] = 100;   
+
+            
+            var o = Activator.CreateInstance(t, constructorParameters);
+
+            
+            object[] parameters = new object[2];
+            parameters[0] = 26;           
+            parameters[1] = "this is a text";   
+
+            
+            var r = methodInfo.Invoke(o, parameters);
+            Console.WriteLine(r);
+
+            var methodInfoStatic = t.GetMethod("StaticMethod");
+            if (methodInfoStatic == null)
             {
-                ParameterInfo[] parameters = mb.GetParameters();
-                if (ParametersMatch(parameters, types))
-                {
-                    return mb;
-                }
+                
+                throw new Exception("InvalidMethoodCallException");
             }
 
-            return null;
-        }
+            
+            object[] staticParameters = new object[2];
+            staticParameters[0] = 12;
+            staticParameters[1] = 3.14159f;
 
-        public override PropertyInfo SelectProperty(
-            BindingFlags bindingAttr,
-            PropertyInfo[] match,
-            Type returnType,
-            Type[] indexes,
-            ParameterModifier[] modifiers)
-        {
-            if (match == null)
-            {
-                throw new ArgumentNullException("match");
-            }
-            foreach (PropertyInfo pi in match)
-            {
-                if (pi.GetType() == returnType &&
-                    ParametersMatch(pi.GetIndexParameters(), indexes))
-                {
-                    return pi;
-                }
-            }
-            return null;
-        }
+            
+            methodInfoStatic.Invoke(o, staticParameters);
 
-        public override object ChangeType(
-            object value,
-            Type myChangeType,
-            CultureInfo culture)
-        {
-            try
-            {
-                object newType;
-                newType = Convert.ChangeType(value, myChangeType);
-                return newType;
-            }
-            // Throw an InvalidCastException if the conversion cannot
-            // be done by the Convert.ChangeType method.
-            catch (InvalidCastException)
-            {
-                return null;
-            }
-        }
-
-        public override void ReorderArgumentArray(ref object[] args,
-            object state)
-        {
-            // No operation is needed here because BindToMethod does not
-            // reorder the args array. The most common implementation
-            // of this method is shown below.
-
-            // ((BinderState)state).args.CopyTo(args, 0);
-        }
-
-        // Returns true only if the type of each object in a matches
-        // the type of each corresponding object in b.
-        private bool ParametersMatch(ParameterInfo[] a, object[] b)
-        {
-            if (a.Length != b.Length)
-            {
-                return false;
-            }
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (a[i].ParameterType != b[i].GetType())
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        // Returns true only if the type of each object in a matches
-        // the type of each corresponding entry in b.
-        private bool ParametersMatch(ParameterInfo[] a, Type[] b)
-        {
-            if (a.Length != b.Length)
-            {
-                return false;
-            }
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (a[i].ParameterType != b[i])
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
